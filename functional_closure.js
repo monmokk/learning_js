d90bc971d24aca0071bf19af5425a7818310b635
monmokk <myeongjikang910814@gmail.com>
2022-07-04 17:52:55 +0900

    ENH - functional closure

--- functional_closure.js
+++ functional_closure.js
@@ -1,11 +1,11 @@
-function makeCounter(aux) {
+const counter = (function () {
     let counter = 0;
 
-    return function () {
+    return function (aux) {
         counter = aux(counter);
         return counter;
     };
-}
+}());
 
 function increase(n) {
     return ++n;
@@ -15,11 +15,9 @@ function decrease(n) {
     return --n;
 }
 
-const increaser = makeCounter(increase);
-console.log(increaser());
-console.log(increaser());
+console.log(counter(increase));
+console.log(counter(increase));
 
 // incre_ 와는 별개의 독립된 렉시컬 환경을 갖기 때문에 카운터 상태가 연동되지 않움.
-const decreaser = makeCounter(decrease);
-console.log(decreaser());
-console.log(decreaser());
+console.log(counter(decrease));
+console.log(counter(decrease));
