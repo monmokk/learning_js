a593e495d961cafc251e877bf45d34e4b043a38f
monmokk <myeongjikang910814@gmail.com>
2022-07-06 13:09:52 +0900

    ENH - async

---
+++ async.js
@@ -0,0 +1,24 @@
+async function foo(n) {
+    return n;
+}
+
+foo(1).then(v => console.log(v));
+
+const bar = async function (n) {
+    return n;
+};
+bar(2).then(v => console.log(v));
+
+const baz = async n => n;
+baz(3).then(v => console.log(v));
+
+const obj = {
+    async foo(n) { return n; }
+}
+obj.foo(4).then(v => console.log(v));
+
+class MyClass {
+    async bar(n) {return n;}
+}
+const myClass = new MyClass();
+myClass.bar(5).then(v => console.log(v));
