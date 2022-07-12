b748ebf04ec1bd9eb782c0f494972266392911d9
monmokk <myeongjikang910814@gmail.com>
2022-07-12 13:33:16 +0900

    linkedList

---
+++ Comparator.js
@@ -0,0 +1,73 @@
+export default class Comparator {
+    /**
+     * Constructor.
+     * @param {function(a: *, b: *)} [compareFunction]
+     * say may compare custom objects together.
+     */
+    constructor(compareFunction) {
+        this.compare = compareFunction || Comparator.defaultCompareFunction;
+    }
+
+    /**
+     * @param {(string|number)} a
+     * @param {(string|number)} b
+     * @returns {number}
+     */
+    static defaultCompareFunction(a, b) {
+        if (a === b) {
+            return 0;
+        }
+
+        return a < b ? -1 : 1;
+    }
+
+    /**
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    equal(a, b) {
+        return this.compare(a, b) === 0;
+    }
+
+    /**
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    lessThan(a, b) {
+        return this.compare(a, b) < 0;
+    }
+
+    /**
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    greaterThan(a, b) {
+        return this.compare(a, b) > 0;
+    }
+
+    /**
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    lessThanOrEqual(a, b) {
+        return this.lessThan(a, b) || this.equal(a, b);
+    }
+
+    /**
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    greaterThanOrEqual(a, b) {
+        return this.greaterThan(a, b) || this.equal(a, b);
+    }
+
+    reverse() {
+        const compareOriginal = this.compare;
+        this.compare = (a, b) => compareOriginal(b, a);
+    }
+}
---
+++ linkedList.js
@@ -0,0 +1,250 @@
+import LinkedListNode from './LinkedListNode';
+import Comparator from './Comparator';
+
+
+
+export default class LinkedList {
+    /**
+     * @param {Function} [comparatorFunction]
+     */
+    constructor(comparatorFunction) {
+        this.head = null;
+        this.tail = null;
+        this.compare = new Comparator(comparatorFunction);
+    }
+
+    /**
+     * @param {*} value
+     * @return {LinkedList}
+     */
+    prepend(value) {
+        const newNode = new LinkedListNode(value, this.head);
+        this.head = newNode;
+
+        if (!this.tail) {
+            this.tail = newNode;
+        }
+
+        return this;
+    }
+
+    /**
+     * @param {*} value
+     * @return {LinkedList}
+     */
+    append(value) {
+        const newNode = new LinkedListNode(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+
+        return this;
+    }
+
+    /**
+     * @param {*} value
+     * @param {number} rawIndex
+     * @return {LinkedList}
+     */
+    insert(value, rawIndex) {
+        const index = rawIndex < 0 ? 0 : rawIndex;
+        if (index === 0) {
+            this.prepend(value);
+        } else {
+            let count = 1;
+            let currentNode = this.head;
+            const newNode = new LinkedListNode(value);
+            while (currentNode) {
+                if (count === index) break;
+                currentNode = currentNode.next;
+                count += 1;
+            }
+            if (currentNode) {
+                newNode.next = currentNode.next;
+                currentNode.next = newNode;
+            } else {
+                if (this.tail) {
+                    this.tail.next = newNode;
+                    this.tail = newNode;
+                } else {
+                    this.head = newNode;
+                    this.tail = newNode;
+                }
+            }
+        }
+        return this;
+    }
+
+    /**
+     * @param {*} value
+     * @return {LinkedListNode}
+     */
+    delete(value) {
+        if (!this.head) {
+            return null;
+        }
+
+        let deletedNode = null;
+
+        while (this.head && this.compare.equal(this.head.value, value)) {
+            deletedNode = this.head;
+            this.head = this.head.next;
+        }
+
+        let currentNode = this.head;
+
+        if (currentNode !== null) {
+            while (currentNode.next) {
+                if (this.compare.equal(currentNode.next.value, value)) {
+                    deletedNode = currentNode.next;
+                    currentNode.next = currentNode.next.next;
+                } else {
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+
+        if (this.compare.equal(this.tail.value, value)) {
+            this.tail = currentNode;
+        }
+
+        return deletedNode;
+    }
+
+    /**
+     * @param {Object} findParams
+     * @param {*} findParams.value
+     * @param {function} [findParams.callback]
+     * @return {LinkedListNode}
+     */
+    find({ value = undefined, callback = undefined }) {
+        if (!this.head) {
+            return null;
+        }
+
+        let currentNode = this.head;
+
+        while (currentNode) {
+            if (callback && callback(currentNode.value)) {
+                return currentNode;
+            }
+
+            if (value !== undefined && this.compare.equal(currentNode.value, value)) {
+                return currentNode;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * @return {LinkedListNode}
+     */
+    deleteTail() {
+        const deletedTail = this.tail;
+
+        if (this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+
+            return deletedTail;
+        }
+
+        let currentNode = this.head;
+        while (currentNode.next) {
+            if (!currentNode.next.next) {
+                currentNode.next = null;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+
+        this.tail = currentNode;
+
+        return deletedTail;
+    }
+
+    /**
+     * @return {LinkedListNode}
+     */
+    deleteHead() {
+        if (!this.head) {
+            return null;
+        }
+
+        const deletedHead = this.head;
+
+        if (this.head.next) {
+            this.head = this.head.next;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return deletedHead;
+    }
+
+    /**
+     * @param {*[]} values
+     * @return {LinkedList}
+     */
+    fromArray(values) {
+        values.forEach((value) => this.append(value));
+
+        return this;
+    }
+
+    /**
+     * @return {LinkedListNode[]}
+     */
+    toArray() {
+        const nodes = [];
+
+        let currentNode = this.head;
+        while (currentNode) {
+            nodes.push(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        return nodes;
+    }
+
+    /**
+     * @param {function} [callback]
+     * @return {string}
+     */
+    toString(callback) {
+        return this.toArray().map((node) => node.toString(callback)).toString();
+    }
+
+    /**
+     * @returns {LinkedList}
+     */
+    reverse() {
+        let currNode = this.head;
+        let prevNode = null;
+        let nextNode = null;
+
+        while (currNode) {
+            nextNode = currNode.next;
+
+            currNode.next = prevNode;
+
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        this.tail = this.head;
+        this.head = prevNode;
+
+        return this;
+    }
+}
